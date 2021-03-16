5e60c61c70721cf4d9882d01df774b889909f6b9
leandro brito <leandroabrito89@gmail.com>
2021-03-16 20:09:43 -0300

    add basic style to index page

--- index.php
+++ index.php
@@ -4,24 +4,25 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>Welcome to Doodle</title>
 </head>
 <body>
+  <div class="wrapper index-page">
+    <div class="main-section"> 
 
-  <div class="main-section"> 
+      <div class="logo-container">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+      </div>
 
-    <div class="logo-container">
-      <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+      <div class="search-container">
+        <form action="search.php" method="GET">
+          <input class="search-box" type="text" name="term" placeholder="Enter a search term">
+          <input class="search-button" type="submit" value="Search">
+        </form>
+
+      </div>
     </div>
-
-    <div class="search-container">
-      <form action="search.php" method="GET">
-        <input class="search-box" type="text" name="term" placeholder="Enter a search term">
-        <input class="search-button" type="submit" value="Search">
-      </form>
-
-    </div>
-
   </div>
   
   
