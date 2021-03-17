7d6541bcd3e3168bab651a66181fdf2722ffdb6d
leandro brito <leandroabrito89@gmail.com>
2021-03-17 17:13:50 -0300

    add search page structure + basic style of search container

--- search.php
+++ search.php
@@ -1,3 +1,54 @@
 <?php 
-  echo $_GET["term"];
+  // echo $_GET["term"];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Welcome to Doodle</title>
+</head>
+<body>
+
+  <div class="wrapper">
+
+    <div class="header">
+
+
+      <div class="header-content">
+
+        <div class="logo-container">
+          <a href="index.php">
+            <img src="assets/images/festisite_google.png" alt="">
+          </a>
+        </div>
+
+        <div class="search-container">
+          
+          <form action="search.php" method="GET">
+
+            <div class="search-bar-container">
+
+              <input class="search-box" type="text" name="term">
+              <button>Search</button>
+
+            </div>
+          
+          </form>
+
+        </div>
+
+      </div>
+
+
+    </div>
+
+    
+  </div>
+  
+  
+</body>
+</html>
