fd3baedfc82230649d867df9542082db1cee4f68
leandro brito <leandroabrito89@gmail.com>
2021-03-31 17:12:11 -0300

    add recursive crawling

--- crawl.php
+++ crawl.php
@@ -2,6 +2,12 @@
 
 include ("classes/DomDocumentParser.php");
 
+$alreadyCrawled = array();
+
+$crawling = array();
+
+
+
 function createLink($src, $url) {   
 
   $scheme = parse_url($url)["scheme"]; // http
@@ -14,38 +20,53 @@ function createLink($src, $url) {
     $src = $scheme . "://" . $host . $src;
   }
   else if(substr($src, 0, 1) == "./") {
-    $src = scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+    $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
   }
   else if(substr($src, 0, 3) == "../") {
-    $src = scheme . "://" . $host . "/" . $src;
+    $src = $scheme . "://" . $host . "/" . $src;
   }
   else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
-    $src = scheme . "://" . $host . "/" . $src;
+    $src = $scheme . "://" . $host . "/" . $src;
   } 
 
   return $src;
 }
 
 function followLinks($url) {
+
+  global $alreadyCrawled;
+  global $crawling;  
   
-    $parser = new DomDocumentParser($url);
+  $parser = new DomDocumentParser($url);
 
-    $linkList = $parser->getLinks();
+  $linkList = $parser->getLinks();
 
-    foreach($linkList as $link) {
-      $href = $link->getAttribute("href");
+  foreach($linkList as $link) {
+    $href = $link->getAttribute("href");
 
-      if(strpos($href, "#") !== false) {
-        continue;
-      } 
-      else if(substr($href, 0, 11) == "javascript:") {
-        continue;
-      }
-
-      $href = createLink($href, $url);
-
-      echo $href . "<br>";
+    if(strpos($href, "#") !== false) {
+      continue;
+    } 
+    else if(substr($href, 0, 11) == "javascript:") {
+      continue;
     }
+
+    $href = createLink($href, $url);
+
+    if(!in_array($href, $alreadyCrawled)) {
+      $alreadyCrawled[] = $href;
+      $crawling[] = $href;
+    }
+
+    echo $href . "<br>";
+  }
+
+  array_shift($crawling);
+
+  foreach($crawling as $site) {
+    followLinks($site);
+  }
+
 }
 
 $startUrl = "http://www.bbc.com";
